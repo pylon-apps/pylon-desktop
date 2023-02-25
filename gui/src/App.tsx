41750fdfb4acc7aac29d29241c049db4471a99ea
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-02-25 11:21:05 +0530

    feat(gui): Allow switching between views

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ReactElement, useState } from "react";
 import { invoke } from "@tauri-apps/api";
 import {
   DashboardOutlined,
@@ -13,6 +13,10 @@ import { Layout, Menu, Empty, theme } from "antd";
 import "./App.css";
 import reactLogo from "./assets/react.svg";
 import Dashboard from "./views/Dashboard/Dashboard";
+import ActiveTransfers from "./views/ActiveTransfers/ActiveTransfers";
+import History from "./views/History/History";
+import Settings from "./views/Settings/Settings";
+import About from "./views/About/About";
 
 const { Content, Sider } = Layout;
 
@@ -66,9 +70,24 @@ const items: MenuItem[] = [
  */
 function App(): any {
   const [collapsed, setCollapsed] = useState(false);
+  const [currentView, setCurrentView] = useState("dashboard");
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  /**
+   * The available views that can be displayed in the app.
+   * @date 2/25/2023 - 11:15:59 AM
+   *
+   * @type {Map<string, ReactElement>} A map containing the unique key identifying the view, along with the view component.
+   */
+  const views: Map<string, ReactElement> = new Map([
+    ["dashboard", <Dashboard logo={reactLogo} />],
+    ["active_transfers", <ActiveTransfers />],
+    ["history", <History />],
+    ["settings", <Settings />],
+    ["about", <About />],
+  ]);
 
   // Disable right-click context menu when running in production.
   invoke("is_release_mode").then((res) => {
@@ -78,6 +97,16 @@ function App(): any {
       );
     }
   });
+
+  /**
+   * Sets the current view based on the selected menu item.
+   * @date 2/25/2023 - 11:13:24 AM
+   *
+   * @param {*} e The menu item click event.
+   */
+  const switchView: MenuProps["onClick"] = (e: any) => {
+    setCurrentView(e.key);
+  };
 
   // TODO: don't inline styles
   return (
@@ -93,6 +122,7 @@ function App(): any {
           defaultSelectedKeys={["dashboard"]}
           mode="inline"
           items={items}
+          onClick={switchView}
         />
       </Sider>
 
@@ -101,7 +131,7 @@ function App(): any {
           <div className="View-container">
             {/* This is where our views would be rendered */}
             {/* TODO: Use custom logo */}
-            <Dashboard logo={reactLogo} />
+            {views.get(currentView)}
           </div>
         </Content>
       </Layout>
