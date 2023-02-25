dfd9f6d961e20996ec4f31f7f3476cc898a47e78
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-02-25 19:47:04 +0530

    refactor(gui): Use `Space` component for spacing

--- gui/src/views/About/About.tsx
+++ gui/src/views/About/About.tsx
@@ -1,5 +1,5 @@
 import { HeartTwoTone } from "@ant-design/icons";
-import { Col, Row, Layout, Typography, Descriptions } from "antd";
+import { Col, Row, Typography, Descriptions, Space } from "antd";
 
 import "./About.css";
 
@@ -16,33 +16,40 @@ interface AboutProps {
 
 function About(props: AboutProps) {
   return (
-    <div className="About">
-      <Row justify="space-around" align="middle">
-        <Col span="flex">
-          <img className="About-logo" src={props.logo} alt="logo" />
-        </Col>
-      </Row>
-      <Row justify="space-around" align="middle">
-        <Title>{props.appName}</Title>
-      </Row>
-      <Row justify="space-around" align="middle">
-        <Descriptions column={1} bordered>
-          <Descriptions.Item label="Core Version">
-            {props.coreVersion}
-          </Descriptions.Item>
-          <Descriptions.Item label="GUI Version">
-            {props.guiVersion}
-          </Descriptions.Item>
-          <Descriptions.Item label="Build Date">
-            {props.buildDate}
-          </Descriptions.Item>
-        </Descriptions>
-      </Row>
-      <Row justify="space-around" align="middle">
-        <Text className="About-footer" strong keyboard>
-          Made with <HeartTwoTone twoToneColor="magenta" /> by {props.author}
-        </Text>
-      </Row>
+    <div>
+      <Space
+        direction="vertical"
+        align="center"
+        size="large"
+        style={{ display: "flex" }}
+      >
+        <Row justify="space-around" align="middle">
+          <Col span="flex">
+            <img className="About-logo" src={props.logo} alt="logo" />
+          </Col>
+        </Row>
+        <Row justify="space-around" align="middle">
+          <Title>{props.appName}</Title>
+        </Row>
+        <Row justify="space-around" align="middle">
+          <Descriptions column={1} bordered>
+            <Descriptions.Item label="Core Version">
+              {props.coreVersion}
+            </Descriptions.Item>
+            <Descriptions.Item label="GUI Version">
+              {props.guiVersion}
+            </Descriptions.Item>
+            <Descriptions.Item label="Build Date">
+              {props.buildDate}
+            </Descriptions.Item>
+          </Descriptions>
+        </Row>
+        <Row justify="space-around" align="middle">
+          <Text className="About-footer" strong keyboard>
+            Made with <HeartTwoTone twoToneColor="magenta" /> by {props.author}
+          </Text>
+        </Row>
+      </Space>
     </div>
   );
 }
--- gui/src/views/Dashboard/Dashboard.tsx
+++ gui/src/views/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Button, Row } from "antd";
+import { Button, Row, Space } from "antd";
 import { UploadOutlined, DownloadOutlined } from "@ant-design/icons";
 
 import "./Dashboard.css";
@@ -10,17 +10,26 @@ interface DashboardProps {
 function Dashboard(props: DashboardProps) {
   return (
     <div>
-      <Row justify="space-around" align="middle">
-        <img className="About-logo" src={props.appLogo} alt="logo" />
-      </Row>
-      <Row justify="center" align="middle">
-        <Button className="Dashboard-button" icon={<UploadOutlined />}>
-          Send
-        </Button>
-        <Button className="Dashboard-button" icon={<DownloadOutlined />}>
-          Receive
-        </Button>
-      </Row>
+      <Space
+        direction="vertical"
+        align="center"
+        size="large"
+        style={{ display: "flex" }}
+      >
+        <Row justify="space-around" align="middle">
+          <img className="Dashboard-logo" src={props.appLogo} alt="logo" />
+        </Row>
+        <Row justify="center" align="middle">
+          <Space size="large">
+            <Button className="Dashboard-button" icon={<UploadOutlined />}>
+              Send
+            </Button>
+            <Button className="Dashboard-button" icon={<DownloadOutlined />}>
+              Receive
+            </Button>
+          </Space>
+        </Row>
+      </Space>
     </div>
   );
 }
