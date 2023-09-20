9505d644adb9d17d4fd643a9017ba06e60eea583
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2023-09-20 17:28:15 +0530

    feat: Initialize router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import {createMemoryRouter, RouterProvider} from "react-router-dom";
+
+const router = createMemoryRouter([
+    {
+        path: "/",
+        element: <App/>,
+    }
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
 );
