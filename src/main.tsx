a64110c2a5c3acd366e2499b56242d72bfa1f8a4
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-04-07 18:26:40 +0530

    chore: initialize nextui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <NextUIProvider>
+      <main className="text-foreground bg-background">
+        <App />
+      </main>
+    </NextUIProvider>
+  </React.StrictMode>
 );
