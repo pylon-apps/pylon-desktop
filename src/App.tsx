892c663ebf745c3d0a68494f367a9dc78f3b8216
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-04-11 16:49:53 +0530

    feat: allow toggling between light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Tabs, Tab } from "@nextui-org/react";
+import { Tabs, Tab, Button } from "@nextui-org/react";
 import Send from "./views/Send";
 import Receive from "./views/Receive";
-import { TbDownload, TbUpload } from "react-icons/tb";
+import { TbDownload, TbUpload, TbSun, TbMoon } from "react-icons/tb";
+import { ReactNode, useState } from "react";
+
+type Theme = "light" | "dark";
+type ThemeIcon = ReactNode;
 
 function App() {
+  const [theme, setTheme] = useState<Theme>("light");
+  const [themeIcon, setThemeIcon] = useState<ThemeIcon>(<TbMoon />);
+
+  const toggleTheme = function () {
+    if (theme == "light") {
+      setTheme("dark");
+      setThemeIcon(<TbSun />);
+    } else {
+      setTheme("light");
+      setThemeIcon(<TbMoon />);
+    }
+  };
+
   return (
-    <main className="text-foreground bg-background">
+    <main className={`${theme} text-foreground bg-background`}>
       <div className="h-screen flex flex-wrap flex-col items-center p-2">
         <Tabs key="options" color="primary" radius="md" aria-label="Options">
           <Tab
@@ -34,6 +51,18 @@ function App() {
             <Receive />
           </Tab>
         </Tabs>
+
+        {/* TODO: make theme selection persistent */}
+        {/* TODO: autodetect initial theme from system */}
+        <Button
+          isIconOnly
+          disableRipple
+          onClick={toggleTheme}
+          className="absolute bottom-2 left-2"
+          aria-label="Toggle theme"
+        >
+          {themeIcon}
+        </Button>
       </div>
     </main>
   );
