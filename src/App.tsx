20b5ccfad3d65a99291a99f7c6e125d5e22fa8f2
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-04-11 14:25:09 +0530

    feat: add tab icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import { Tabs, Tab } from "@nextui-org/react";
 import Send from "./views/Send";
 import Receive from "./views/Receive";
+import { TbDownload, TbUpload } from "react-icons/tb";
 
 function App() {
   return (
     <div className="h-screen flex flex-wrap flex-col items-center p-2">
-      {/* TODO: add icons to tabs */}
       <Tabs key="options" color="primary" radius="md" aria-label="Options">
-        <Tab key="send" title="Send" className="w-full h-5/6">
+        <Tab
+          key="send"
+          className="w-full h-5/6"
+          title={
+            <div className="flex items-center space-x-2">
+              <TbUpload />
+              <span>Send</span>
+            </div>
+          }
+        >
           <Send />
         </Tab>
-        <Tab key="receive" title="Receive" className="w-full h-5/6">
+        <Tab
+          key="receive"
+          className="w-full h-5/6"
+          title={
+            <div className="flex items-center space-x-2">
+              <TbDownload />
+              <span>Receive</span>
+            </div>
+          }
+        >
           <Receive />
         </Tab>
       </Tabs>
