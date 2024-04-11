a5d990f95febde4264de0ff0e4062300a4ea7101
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-04-11 22:07:50 +0530

    style: use consistent quotes style

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
 	// detect user language
@@ -12,7 +12,7 @@ i18n
 	// for all options read: https://www.i18next.com/overview/configuration-options
 	.init({
 		debug: true,
-		fallbackLng: 'en',
+		fallbackLng: "en",
 		interpolation: {
 			escapeValue: false, // not needed for react as it escapes by default
 		},
