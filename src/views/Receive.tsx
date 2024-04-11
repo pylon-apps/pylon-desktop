71eaf2547bf6f54c4591f6a14bdbe8f7c7931f22
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-04-11 15:33:42 +0530

    feat: design initial send and receive views

--- src/views/Receive.tsx
+++ src/views/Receive.tsx
@@ -1,8 +1,28 @@
+import { Button, Input, Spacer } from "@nextui-org/react";
+import { TbDownload } from "react-icons/tb";
+
 function Receive() {
   return (
-    // TODO: implement view
-    <div className="flex flex-col justify-center items-center h-full">
-      <h1>Receive</h1>
+    <div className="flex flex-col justify-center items-center h-full space-y-1">
+      <TbDownload className="text-9xl text-foreground-500" />
+
+      <Spacer y={4} />
+
+      <span className="text-xl font-extrabold text-foreground">
+        Receive File
+      </span>
+      <span className="text-sm font-light text-foreground-500">
+        Enter the Pylon code to receive the file or folder from the sender
+      </span>
+
+      <Spacer y={4} />
+
+      <div className="flex flex-row space-x-2 w-3/5">
+        <Input label="Pylon Code" size="sm" />
+        <Button color="primary" className="self-center">
+          Receive
+        </Button>
+      </div>
     </div>
   );
 }
--- src/views/Send.tsx
+++ src/views/Send.tsx
@@ -1,8 +1,43 @@
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Spacer,
+} from "@nextui-org/react";
+import { TbChevronDown, TbFile, TbFolder, TbUpload } from "react-icons/tb";
+
 function Send() {
   return (
-    // TODO: implement view
-    <div className="flex flex-col justify-center items-center h-full">
-      <h1>Send</h1>
+    <div className="flex flex-col justify-center items-center h-full space-y-1">
+      <TbUpload className="text-9xl text-foreground-500" />
+
+      <Spacer y={4} />
+
+      <span className="text-xl font-extrabold text-foreground">Send File</span>
+      <span className="text-sm font-light text-foreground-500">
+        Select the file or folder to send
+      </span>
+
+      <Spacer y={4} />
+
+      <Dropdown>
+        <DropdownTrigger>
+          <Button color="primary" endContent={<TbChevronDown />}>
+            Select
+          </Button>
+        </DropdownTrigger>
+
+        <DropdownMenu aria-label="Select">
+          <DropdownItem key="file" startContent={<TbFile />}>
+            File
+          </DropdownItem>
+          <DropdownItem key="folder" startContent={<TbFolder />}>
+            Folder
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 }
