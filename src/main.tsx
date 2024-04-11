4881d2522e79053be8202610277505cb3b0f0a36
Nikhil Prabhu <nikhilprabhu98@gmail.com>
2024-04-11 15:38:53 +0530

    feat: use custom font

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { NextUIProvider } from "@nextui-org/react";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className="text-foreground bg-background">
+      <main className="dark text-foreground bg-background">
         <App />
       </main>
     </NextUIProvider>
